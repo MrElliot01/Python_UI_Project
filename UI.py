14b4ea3071a3425439944b9a348d9ce80c1c20ca
Elliot.Shuck <elliot.shuck@speakerbus.com>
2022-02-15 18:10:51 +0000

    File Tidy up Added some formatting to the buttons and the path name for some of the png elements

--- UI.py
+++ UI.py
@@ -1,7 +1,8 @@
 import tkinter as tk
 import PyPDF2
+# pillow module
 from PIL import Image, ImageTk
-from tkinter.filedialog import askopenfile 
+from tkinter.filedialog import askopenfile
 
 # Begining of the interface
 root = tk.Tk()
@@ -12,7 +13,6 @@ root = tk.Tk()
 canvas = tk.Canvas(root, width=600, height=500)
 # Gives us three idivudal colums to work with
 canvas.grid(columnspan=3, rowspan=3)
-
 
 # Gets the logo from the file directory
 logo = Image.open('./starterFiles/logo.png')
@@ -25,8 +25,6 @@ logo_label.image = logo
 # Adds the logo to the center as we have three colums 0,1,2
 logo_label.grid(column=1, row=0)
 
-
-
 # instructions
 instructions = tk.Label(root, text="Select a PDF file on your computer to extract all its text", font="Raleway")
 # Positions the instuctions
@@ -38,7 +36,7 @@ def open_file():
     # Text changes when the button is clicked
     browse_text.set("loading...")
     # Searches for the file, we have defined here that it must be a PDF file
-    file = askopenfile(parent=root, mode='rb', title="Choosea file", filetypes=[("Pdf file","*.pdf")])
+    file = askopenfile(parent=root, mode='rb', title="Choosea file", filetypes=[("Pdf file", "*.pdf")])
     # if the file is found
     if file:
         print("file was sucessfuly loaded")
@@ -52,21 +50,20 @@ def open_file():
     text_box = tk.Text(root, height=10, width=50, padx=15, pady=15)
     # Text index, content
     text_box.insert(1.0, page_content)
+    # location
+    text_box.tag_configure("center", justify="center")
+    text_box.tag_add("center", 1.0, "end")
     text_box.grid(column=1, row=3)
+
+    browse_text.set("Browse")
+
 
 # Browse button
 browse_text = tk.StringVar()
-browse_btn = tk.Button(root, textvariable=browse_text, command=lambda:open_file(), font="Raleway", bg='#20bebe', fg="white", height=2, width=15)
+browse_btn = tk.Button(root, textvariable=browse_text, command=lambda: open_file(), font="Raleway", bg='#20bebe',
+                       fg="white", height=2, width=15)
 browse_text.set("Browse")
 browse_btn.grid(column=1, row=2)
-
-
-
-
-
-
-
-
 
 # everything we create in the UImust be between root = tk.Tk() and root.mainloop()
 root.mainloop()
